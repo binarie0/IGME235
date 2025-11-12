50d4aee2b2c20968d3c2df3c00ab162f42ba6762
Zach <80496561+binarie0@users.noreply.github.com>
2025-11-12 16:41:56 -0500

    finish circle blast part 2

--- 235/circle-blast/js/main.js
+++ 235/circle-blast/js/main.js
@@ -89,6 +89,7 @@ fireballSound = new Howl({
   // #7 - Load sprite sheet
 
   // #8 - Start update loop
+  app.ticker.add(gameLoop);
 
   // #9 - Start listening for click events on the canvas
 
@@ -161,6 +162,25 @@ playAgainButton.on("pointerover", (e) => (e.target.alpha = 0.7)); // concise arr
 playAgainButton.on("pointerout", (e) => (e.currentTarget.alpha = 1.0)); // ditto
 gameOverScene.addChild(playAgainButton);
 
+
+
+  //game function
+  let textStyle = {
+        fill: 0xffffff,
+        fontSize: 18,
+        fontFamily: "Verdana",
+        stroke: 0xff0000,
+        strokeThickness: 4
+    }
+    scoreLabel = new PIXI.Text("", textStyle);
+    scoreLabel.x = 5;
+    scoreLabel.y = 5;
+    gameScene.addChild(scoreLabel);
+
+    lifeLabel = new PIXI.Text("", textStyle);
+    lifeLabel.x = 5;
+    lifeLabel.y = 26;
+    gameScene.addChild(lifeLabel);
 }
 function startGame()
 {
@@ -168,26 +188,13 @@ function startGame()
     gameScene.visible = true;
     gameOverScene.visible = false;
 
-
-    let textStyle = {
-        fill: 0xffffff,
-        fontSize: 18,
-        fontFamily: "Verdana",
-        stroke: 0xff0000,
-        strokeThickness: 4
-    }
-
-    scoreLabel = new PIXI.Text("", textStyle);
-    scoreLabel.x = 5;
-    scoreLabel.y = 5;
-    gameScene.addChild(scoreLabel);
     increaseScoreBy(0);
-
-    lifeLabel = new PIXI.Text("", textStyle);
-    lifeLabel.x = 5;
-    lifeLabel.y = 26;
-    gameScene.addChild(lifeLabel);
     decreaseLifeBy(0);
+
+
+
+    loadLevel();
+    setTimeout(() => paused = false, 50);
 }
 
 function increaseScoreBy(amount)
@@ -201,4 +208,107 @@ function decreaseLifeBy(amount)
     life -= amount;
     life = parseInt(life);
     lifeLabel.text = `Life:  ${life}%`;
+}
+
+function gameLoop(){
+   if (paused) return; // keep this commented out for now
+
+  // #1 - Calculate "delta time"
+let dt = 1 / app.ticker.FPS;
+if (dt > 1 / 12) dt = 1 / 12;
+
+
+  // #2 - Move Ship
+let mousePosition = app.renderer.events.pointer.global;
+//ship.position = mousePosition;
+
+  let amt = 2*dt;
+  let newX = lerp(ship.x, mousePosition.x, amt);
+  let newY = lerp(ship.y, mousePosition.y, amt);
+
+  let w2 = ship.width * ship.scale.x / 2;
+  let h2 = ship.height * ship.scale.y / 2;
+  ship.x = clamp(newX, 0 + w2, sceneWidth - w2);
+  ship.y = clamp(newY, 0 + h2, sceneHeight - h2);
+
+  // #3 - Move Circles
+  for (let c of circles)
+  {
+    c.move(dt);
+    if (c.x <= c.radius || c.x >= sceneWidth - c.radius)
+    {
+      c.reflectX();
+      c.move(dt);
+    }
+    if (c.y <= c.radius || c.y >= sceneHeight - c.radius)
+    {
+      c.reflectY();
+      c.move(dt);
+    }
+    
+    //#5 happening here to save a little bit of looping
+    if (c.isAlive && rectsIntersect(c, ship))
+    {
+      hitSound.play();
+      gameScene.removeChild(c);
+      c.isAlive = false;
+      decreaseLifeBy(20);
+    }
+
+
+
+    
+  }
+
+  // #4 - Move Bullets
+
+  // #6 - Now do some clean up
+
+  circles = circles.filter((c) => c.isAlive);
+
+
+  // #7 - Is game over?
+if (life <= 0){
+  end();
+  return; // return here so we skip #8 below
+}
+
+
+  // #8 - Load next level
+}
+
+
+
+function createCircles(numCircles = 10)
+{
+  for (let i = 0; i < numCircles; i++)
+  {
+    let c = new Circle(10, 0xffff00);
+    c.x = Math.random() * (sceneWidth - 50) + 25;
+    c.y = Math.random() * (sceneHeight - 400) + 25;
+    circles.push(c);
+    gameScene.addChild(c);
+  }
+
+}
+
+function loadLevel(){
+  createCircles(levelNum * 5);
+
+  life = 100;
+  decreaseLifeBy(0);
+  score = 0;
+  increaseScoreBy(0);
+  ship.x = 300;
+  ship.y = 550;
+}
+
+function end()
+{
+  paused = true;
+  circles.forEach((c) => gameScene.removeChild(c));
+  circles = [];
+
+  gameOverScene.visible = true;
+  gameScene.isVisible = false;
 }
